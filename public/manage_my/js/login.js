3dd079a482b82b7da4ba03fee764509396211bff
msi <msi>
2017-12-07 16:37:30 +0800

    添加用户密码判定 增加精度条插件

--- public/manage_my/js/login.js
+++ public/manage_my/js/login.js
@@ -28,7 +28,15 @@ $(function () {
                     notEmpty: {
                         message: '用户名不能为空'
                     },
-                    
+                    //长度校验
+                    stringLength: {
+                        min: 3,
+                        max: 12,
+                        message: '用户名长度必须在3到12之间'
+                    },
+                    callback: {
+                        message: '用户名不正确'
+                    }
 
                 }
             },
@@ -44,10 +52,45 @@ $(function () {
                         max: 16,
                         message: '用户名长度必须在6到30之间'
                     },
+                    callback: {
+                        message: '密码错误'
+                    }
 
                 }
             }
         }
+    }).on('success.form.bv', function (e) {
+        e.preventDefault();
+        // 开启精度条
+        NProgress.start();
+        $.ajax({
+            url: "/employee/employeeLogin",
+            // 创建以标准 URL 编码表示的文本字符串
+            data: $("form").serialize(),
+            type: 'post',
+            success: function (backData) {
+                // console.log(backData);
+                //正确
+                if (backData.success) {
+                    window.location = './index.html';
+                } else {
+                    // 获取验证插件对象
+                    var validator = $("form").data('bootstrapValidator');
+
+                    if (backData.error == 1001) {
+                        validator.updateStatus('password', 'INVALID', 'callback')
+
+                    } else if (backData.error == 1000) {
+                        validator.updateStatus('username', 'INVALID', 'callback')
+                    }
+
+                }
+                setTimeout(function () {
+                    // 收起进度条
+                    NProgress.done();
+                }, 1000)
+            }
+        })
     })
 
 })
