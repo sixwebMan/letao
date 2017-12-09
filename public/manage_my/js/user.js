3c95cdde81fc4da2b4781a09c7a732a73724f80b
msi <msi>
2017-12-09 20:45:20 +0800

    链接ajax 提取用户数据  建立模板引擎 渲染页面

--- public/manage_my/js/user.js
+++ public/manage_my/js/user.js
@@ -1,3 +1,22 @@
 $(function () {
+    var page = 1;
+    var pageSize = 5;
 
+    function getData() {
+        $.ajax({
+            url: '/user/queryUser',
+            data: {
+                page: page,
+                pageSize: pageSize,
+            },
+            success: function (backData) {
+                // console.log(backData);
+                // 渲染模板引擎
+                $('tbody').html(template('user',backData));
+                // 需求2 插入分页
+            }
+        })
+    }
+
+    getData();
 })
