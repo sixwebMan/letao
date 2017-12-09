5659916a79d8ba85a488745a272f22768f22f386
msi <msi>
2017-12-09 21:25:44 +0800

    给用户页面添加页码插件,设置点击启用 禁用事件 用户页面功能完成

--- public/manage_my/js/user.js
+++ public/manage_my/js/user.js
@@ -1,22 +1,64 @@
 $(function () {
-    var page = 1;
+    var pageNum = 1;
     var pageSize = 5;
+
 
     function getData() {
         $.ajax({
             url: '/user/queryUser',
             data: {
-                page: page,
+                page: pageNum,
                 pageSize: pageSize,
             },
             success: function (backData) {
-                // console.log(backData);
+                console.log(backData);
                 // 渲染模板引擎
-                $('tbody').html(template('user',backData));
+                $('tbody').html(template('user', backData));
                 // 需求2 插入分页
+                $("#pagintor").bootstrapPaginator({
+                    bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: pageNum, //当前页
+                    totalPages: Math.ceil(backData.total / backData.size), //总页数
+                    size: "small", //设置控件的大小，mini, small, normal,large
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        //   console.log(page);
+                        pageNum = page;
+                        getData();
+
+                    }
+                });
             }
         })
     }
 
     getData();
+
+    // 给禁用启用按钮提供点击事件
+    $('tbody').on('click', 'button', function () {
+        // console.log('click');
+        // 获取上传的自定义属性
+        var myId = $(this).parent().attr('data-id');
+        //   console.log(id)
+        var isDelete = undefined;
+        if ($(this).html()=='启用') {
+            // 如果是启用的改为禁用
+            isDelete = 0;
+        }else{
+            isDelete = 1;
+        }
+        $.ajax({
+            url:'/user/updateUser',
+            data:{
+                id:myId,
+                isDelete:isDelete
+            },
+            type:'post',
+            success: function (backData) {
+
+                getData();
+              }
+
+        })
+    })
 })
